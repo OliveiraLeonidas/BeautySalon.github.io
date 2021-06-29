97c6104e363a5b2a6b3c7716eea952ae07426634
OliveiraLeonidas <leonidasoliv25@gmail.com>
2021-06-29 19:33:41 -0300

    lesson5

--- Evento nlw - Rockeseat/main.js
+++ Evento nlw - Rockeseat/main.js
@@ -51,12 +51,18 @@ function changeHeaderWhenScroll() {
 /*========= Testemonials carousel sliders swiper ========================== */
 
 const swiper = new Swiper('.swiper-container', {
-  slidePerView: 1,
+  slidePerView: 2,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidePerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* scrollReveal - revelar quando der scroll na página*/
@@ -91,8 +97,36 @@ function backToTop() {
   }
 }
 
+/* abrir menu se seção visivel na página */
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSelection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+      querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove(
+        'active'
+      )
+    }
+  }
+}
+
 /*==== When scroll ==================== */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSelection()
 })
